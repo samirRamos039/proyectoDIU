7b5837974fa6c80e393582487d0eb9b25ed42992
samirRamos039 <samir.ramos@correounivalle.edu.co>
2022-01-02 08:41:44 -0500

    clase conexion

---
+++ ferplasticos/src/modelo/Conexion.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author rzagza
+ */
+public class Conexion {
+    
+    
+     private final String DRIVER = "org.postgresql.Driver";
+    private final String URL = "jdbc:postgresql://localhost:5432/sistemaventas";
+    private final String USER = "postgres";
+    private final String PASSWORD = "1234";
+    private Connection con;
+
+    public Connection conectar () {
+        
+        
+        try {
+            Class.forName(DRIVER);
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e){
+              e.printStackTrace();
+        }
+        return con;
+    }
+    
+}
